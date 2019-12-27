86fb157904711219d284b8db3588a8d21413838c
abourass <abourassa@assetval.com>
2019-12-27 09:48:22 -0700

    Updating libraries

--- src/micro.js
+++ src/micro.js
@@ -1,13 +1,3 @@
-/* ==========================================================================
-//                               µ Functions                               //
-=============================================================================
-                              TABLE OF CONTENTS
- =============================================================================
- 0. µ - Grab whatever element on a page you need (Uses querySelector)
- 1. µAll - Grab All Selectors on the page (Uses querySelectorAll)
- ===========================================================================*/
-
-// 00 - Grab whatever element on a page you need (Uses querySelector)
 export const µ = function(selector) {
   let el;
   const obj = {
@@ -20,7 +10,13 @@ export const µ = function(selector) {
       return this;
     },
     addClass(className) {
-      el.classList.add(className);
+      if (Array.isArray(className)){
+        className.forEach((singleClass) => {
+          el.classList.add(singleClass);
+        });
+      } else {
+        el.classList.add(className);
+      }
       return this;
     },
     removeClass(className) {
@@ -28,11 +24,11 @@ export const µ = function(selector) {
       return this;
     },
     hasClass(className){
-      return el.classList.contains(className)
+      return el.classList.contains(className);
     },
     css(arrayOrObject){
       if (Array.isArray(arrayOrObject)){
-        arrayOrObject.forEach(obj => { el.style[Object.keys(obj)[0]] = obj.value; })
+        arrayOrObject.forEach((obj) => { el.style[Object.keys(obj)[0]] = obj.value; });
       } else {
         el.style[Object.keys(arrayOrObject)[0]] = arrayOrObject.value;
       }
@@ -45,10 +41,9 @@ export const µ = function(selector) {
     html(string) {
       if (!string) {
         return el.innerHTML;
-      } else {
-        el.innerHTML = string;
-        return this;
       }
+      el.innerHTML = string;
+      return this;
     },
     empty(){
       el.innerHTML = null;
@@ -68,16 +63,16 @@ export const µ = function(selector) {
       el.parentNode.removeChild(el);
       return this;
     },
-    set(obj) {
-      Object.keys(obj).forEach((key) => {
-        el.setAttribute(key, obj[key])
+    set(setObj) {
+      Object.keys(setObj).forEach((key) => { // eslint-disable-next-line security/detect-object-injection
+        el.setAttribute(key, setObj[key]);
       });
       return this;
     },
     child(element, insertAt = null){
       if (insertAt === 'append' || insertAt == null) { el.append(element); }
-      if (insertAt === 'prepend'){ el.prepend(element) }
-      return this
+      if (insertAt === 'prepend'){ el.prepend(element); }
+      return this;
     },
     text(txt){
       el.textContent = txt.toString();
@@ -86,7 +81,7 @@ export const µ = function(selector) {
     textChild(string){
       const textEl = document.createTextNode(string.toString());
       el.appendChild(textEl);
-      return this
+      return this;
     },
     type(type){
       el.type = type;
@@ -106,23 +101,23 @@ export const µ = function(selector) {
       return this;
     },
     find(element){
-      return el.querySelectorAll(element)
+      return el.querySelectorAll(element);
     },
     siblings(){
-      return Array.from(el.parentNode.children).filter((child) => child !== el);
+      return Array.from(el.parentNode.children).filter(child => child !== el);
     },
     previous(){
       return el.previousElementSibling;
     },
     next(){
-      return el.nextElementSibling
+      return el.nextElementSibling;
     },
     val(newVal){
       el.value = newVal;
       return this;
     },
     data(dataSuffix){
-      return el.getAttribute(`data-${dataSuffix}`)
+      return el.getAttribute(`data-${dataSuffix}`);
     },
     height(){
       const styles = window.getComputedStyle(el);
@@ -143,7 +138,7 @@ export const µ = function(selector) {
       return width - borderBottomWidth - borderTopWidth - paddingTop - paddingBottom;
     },
     diameter(){
-      return {width: el.scrollWidth, height: el.scrollHeight}
+      return {width: el.scrollWidth, height: el.scrollHeight};
     },
     stats(){
       const box = el.getBoundingClientRect();
@@ -164,24 +159,24 @@ export const µ = function(selector) {
         heightRestricted: restrictedHeight,
         widthRestricted: restrictedWidth,
         height: el.scrollHeight,
-        width: el.scrollWidth
-      }
+        width: el.scrollWidth,
+      };
     },
     position(){
-      return {left: el.offsetLeft, top: el.offsetTop}
+      return {left: el.offsetLeft, top: el.offsetTop};
     },
     offset(){
       const box = el.getBoundingClientRect();
       return {
         top: box.top + window.pageYOffset - document.documentElement.clientTop,
-        left: box.left + window.pageXOffset - document.documentElement.clientLeft
+        left: box.left + window.pageXOffset - document.documentElement.clientLeft,
       };
     },
     load(url, completeCallback = null){
       if (completeCallback == null){
-        fetch(url).then(data => data.text()).then(data => { el.innerHTML = data });
+        fetch(url).then(data => data.text()).then((data) => { el.innerHTML = data; });
       } else {
-        fetch(url).then(data => data.text()).then(data => { el.innerHTML = data }).then(completeCallback)
+        fetch(url).then(data => data.text()).then((data) => { el.innerHTML = data; }).then(completeCallback);
       }
       return this;
     },
@@ -191,19 +186,18 @@ export const µ = function(selector) {
     },
     off(eventName, eventHandler) {
       el.removeEventListener(eventName, eventHandler);
-    }
+    },
   };
   el = obj.grab(selector);
   return obj;
 };
 
-// 01 - Grab All Selectors on the page (Uses querySelectorAll)
 export const µAll = async(selector) => {
   let el;
   const obj = {
     grabAll(){
-      if (el) return el;
-      return document.querySelectorAll(selector)
+      if (el){ return el; }
+      return document.querySelectorAll(selector);
     },
     toggleClass(className) {
       el.forEach((singleEl) => { singleEl.classList.toggle(className); });
@@ -226,7 +220,7 @@ export const µAll = async(selector) => {
       return this;
     },
     remove() {
-      el.forEach((singleEl) => { singleEl.parentNode.removeChild(el);});
+      el.forEach((singleEl) => { singleEl.parentNode.removeChild(el); });
       return this;
     },
     text(string) {
@@ -234,15 +228,15 @@ export const µAll = async(selector) => {
       return this;
     },
     type(type){
-      el.forEach((singleEl) => { singleEl.type = type;});
+      el.forEach((singleEl) => { singleEl.type = type; });
       return this;
     },
     name(name){
-      el.forEach((singleEl) => { singleEl.name = name;});
+      el.forEach((singleEl) => { singleEl.name = name; });
       return this;
     },
     intro({name, type} = {}){
-      el.forEach((singleEl) => { singleEl.name = name; singleEl.type = type;});
+      el.forEach((singleEl) => { singleEl.name = name; singleEl.type = type; });
       return this;
     },
     wrap(classForDivs){
@@ -252,96 +246,26 @@ export const µAll = async(selector) => {
         element.parentNode.insertBefore(wrapper, element);
         element.parentNode.removeChild(element);
         wrapper.appendChild(element);
-      })
+      });
     },
     load(url, completeCallback = null){
       if (completeCallback == null){
-        el.forEach((element) => {fetch(url).then(data => data.text()).then(data => { element.innerHTML = data });})
+        el.forEach((element) => { fetch(url).then(data => data.text()).then((data) => { element.innerHTML = data; }); });
       } else {
-        el.forEach((element) => {fetch(url).then(data => data.text()).then(data => { element.innerHTML = data }).then(completeCallback)})
+        el.forEach((element) => { fetch(url).then(data => data.text()).then((data) => { element.innerHTML = data; }).then(completeCallback); });
       }
       return this;
     },
     on(eventName, eventHandler){
-      el.forEach((element) => {element.addEventListener(eventName, eventHandler);});
+      el.forEach((element) => { element.addEventListener(eventName, eventHandler); });
       return this;
     },
     off(eventName, eventHandler){
-      el.forEach((element) => {element.removeEventListener(eventName, eventHandler);})
-    }
+      el.forEach((element) => { element.removeEventListener(eventName, eventHandler); });
+    },
   };
   el = obj.grabAll(selector);
   return obj;
 };
 
-export const Ω = function(element, idForNewElement, appendTo) {
-  const newEl = document.createElement(element);
-  newEl.id = idForNewElement;
-  document.querySelector(appendTo).appendChild(newEl);
-  return µ(`#${idForNewElement}`);
-};
-
-export const ΩMany = function(arrayOfObjects){
-  arrayOfObjects.forEach(obj => {
-    const newEl = document.createElement(obj.element);
-    newEl.id = obj.idForNewElement;
-    document.querySelector(obj.appendTo).appendChild(newEl)
-  })
-};
-
-export const ΩCSS = function(styles){
-  const css = document.createElement('style'); css.type = 'text/css';
-  if (css.styleSheet){ css.styleSheet.cssText = styles; } else { css.appendChild(document.createTextNode(styles)); }
-  document.getElementsByTagName('head')[0].appendChild(css);
-};
-
-export const pageDone = function(cb){ // Replaces jQuery document ready
-  if (document.readyState === 'complete' || document.readyState !== 'loading') {
-    cb();
-  } else { document.addEventListener('DOMContentLoaded', cb); }
-};
-
-export const windowHeight = ({scrollbar}={}) => {
-  if (scrollbar){ return window.document.documentElement.clientHeight; }
-  return window.innerHeight
-};
-
-export const windowWidth = ({scrollbar}={}) => {
-  if (scrollbar){ return window.document.documentElement.clientWidth; }
-  return window.innerWidth
-};
-
-export const documentHeight = () => {
-  const body = document.body;
-  const html = document.documentElement;
-  return Math.max(
-    body.offsetHeight, body.scrollHeight,
-    html.clientHeight, html.offsetHeight, html.scrollHeight
-  );
-};
-
-export const documentWidth = () => {
-  const body = document.body;
-  const html = document.documentElement;
-  return Math.max(
-    body.offsetWidth, body.scrollWidth,
-    html.clientWidth, html.offsetWidth, html.scrollWidth
-  );
-};
-
-export const scrollTop = () => { (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop; };
-
-export default {
-  µ,
-  µAll,
-  Ω,
-  ΩMany,
-  ΩCSS,
-  pageDone,
-  windowHeight,
-  windowWidth,
-  documentHeight,
-  documentWidth,
-  scrollTop
-};
-
+export default {µ, µAll};
